e09225796d29048c86fb5e4d82c27277c70415ce
Ricardo <Onydarius@hotmai.com>
2023-05-02 21:23:28 -0600

    Javascrip separated

---
+++ src/main/resources/static/js/optionStateLoader.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    const stateSelect = document.querySelector("#state");
+    const birthStateSelect = document.querySelector("#birth-state");
+
+
+    const states = fetch("/data/data.json")
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            Object.keys(data).forEach(state => {
+                var opt = document.createElement('option');
+                opt.value = state;
+                opt.innerHTML = state;
+                const clone = opt.cloneNode(true);
+                stateSelect.appendChild(opt);
+                birthStateSelect.appendChild(clone);
+
+            });
+            var opt = document.createElement('option');
+            opt.value = "Otro";
+            opt.innerHTML = "Otro";
+            stateSelect.appendChild(opt);
+            birthCitySelect.appendChild(opt);
+
+            return data;
+        });
+})
+
+//const birthCitySelect = document.querySelector("#birth-city");
+//const citySelect = document.querySelector("#city");
